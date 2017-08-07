8a52c010a7f18fe29cd35c4de9f45bbabad49424
ziyuan <ziyuan@2dfire.com>
2017-08-07 22:35:39 +0800

    一些注释
    一些注释巴拉巴拉

--- netty-action/src/main/java/com/shiwuliang/echo/server/EchoServerHandler.java
+++ netty-action/src/main/java/com/shiwuliang/echo/server/EchoServerHandler.java
@@ -19,6 +19,10 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
         ByteBuf buf = (ByteBuf) msg;
         System.out.println(buf.toString(CharsetUtil.UTF_8));
         //这个时候不会写到套接字里，只会放到
+        // if (buf.isWritable()) {
+        //     这里抛出异常，是直接抛出去了，并不会调用exceptionCaught方法。
+        //     throw new NullPointerException("buf is null");
+        // }
         ctx.write(buf);
     }
 
@@ -32,6 +36,14 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
         });
     }
 
+    //经过验证，这个方法并不是channelRead之类的方法之中出现异常调用的，而是
+        // childHandler(new ChannelInitializer<SocketChannel>() {
+
+        //             protected void initChannel(SocketChannel ch) throws Exception {
+        //                 ch.pipeline().addLast(serverHandler);
+        //             }
+        //         });
+    //这个方法中，调用initChannel的时候出现异常时候调用的，有待考证。。
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         //如果不重写这个方法，异常会直接被传递到整个channelPipline的尾端，所以程序应该最少提供一个实现了exceptionCaught的handler
