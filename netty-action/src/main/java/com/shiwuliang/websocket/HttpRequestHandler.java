8a5e7d121da21d33e08845b9e0bc186f5a8b31ec
ziyuan <ziyuan@2dfire.com>
2017-08-28 22:02:50 +0800

    一些注释

--- netty-action/src/main/java/com/shiwuliang/websocket/HttpRequestHandler.java
+++ netty-action/src/main/java/com/shiwuliang/websocket/HttpRequestHandler.java
@@ -41,10 +41,12 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
         if (wsUri.equalsIgnoreCase(request.uri())) {
             //如果请求了WebSocket协议升级，则增加引用计数，并将它传递给下一个ChannelInboundHandler
+            //调用channelRead之后会release这个request
             ctx.fireChannelRead(request.retain());
         } else {
             if (HttpUtil.is100ContinueExpected(request)) {
                 //处理100 continue 请求以符合HTTP 1.1规范
+                //如果客户端发送了 HTTP 1.1的HTTP头信息Expect:100-continue，那么会发送一个100 Continue的响应
                 send100Continue(ctx);
             }
 
@@ -58,16 +60,20 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
                 response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
             }
 
-            //http response写到客户端
+            //http response写到客户端，注意这不是一个FullHttpResponse，只是请求的第一部分，所以没有flush
             ctx.write(response);
+
+            //如故不需要加密和压缩，那么可以通过将index.html的内容存储到DefaultFileRegion中来达到最佳效率，这会利用零拷贝特性来进行内容的传输
+            //否则使用ChunkedNioFile。
             if (ctx.pipeline().get(SslHandler.class) == null) {
-                //将index.html写到客户端
                 ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
             } else {
+
                 ctx.write(new ChunkedNioFile(file.getChannel()));
             }
 
-            //将lastHttpContent冲刷到远端
+            //将lastHttpContent冲刷到远端，标记响应的结束
+            //如果没有请求keep-alive，那么添加一个ChannelFutureListener到最后一次写出动作的ChannelFuture，并关闭连接
             ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
             if (!keepAlive) {
                 //如果没有请求keep-alive，则在写操作完成之后关闭
